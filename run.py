128e8c4f1e0f3341d34afb4d7f6b3e11f16cafe7
Mate290 <42339555+Mate290@users.noreply.github.com>
2023-06-02 05:54:50 -0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DatabaseIot.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5005)
